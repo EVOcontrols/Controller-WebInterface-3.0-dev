ccb329817ce593d81f34e8664190c66f62ac97c7
SI-IC <aleksandr.makarov@evocontrols.ru>
2023-09-13 14:28:22 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './src/assets/img/tailwind/*.svg'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
