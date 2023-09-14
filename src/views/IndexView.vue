c9adb646971b29a1df81cbfede7f9fe739686bee
SI-IC <aleksandr.makarov@evocontrols.ru>
2023-09-14 12:13:31 +0300

    next commit

---
+++ src/views/IndexView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>Hi</div>
+</template>
+
+<script lang="ts" setup></script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,28 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
+import AutoImport from 'unplugin-auto-import/vite';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      dts: true,
+      imports: ['vue', 'vue-router', '@vueuse/core'],
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+      ],
+      defaultExportByFilename: true,
+      vueTemplate: true,
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
