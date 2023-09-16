6fae707bc0ea8b5db3f6c2b99a8d025294fa7799
SI-IC <aleksandr.makarov@evocontrols.ru>
2023-09-16 11:00:35 +0300

    next commit

---
+++ src/components/Ui/LoginInput.vue
@@ -0,0 +1,243 @@
+<template>
+  <div class="relative">
+    <input
+      :type="inputType === 'login' || isPasswordVisible ? 'text' : 'password'"
+      autocorrect="off"
+      autocapitalize="off"
+      aria-invalid="false"
+      class="absolute left-0 top-0 z-[2] h-full w-full pl-4 text-sm opacity-0"
+      :class="{ 'top-10 opacity-100': inputType === 'password1', 'pl-5': isInputFocus }"
+      v-model="value"
+      name="type"
+      :autocomplete="autocomplete"
+      :disabled="isDisabled"
+      @focus="gotFocus"
+      @blur="onBlur"
+      @click="isFirefox ? gotFocus() : ''"
+      @keydown="isFirefox ? gotFocus() : ''"
+      @keyup="isFirefox ? gotFocus() : ''"
+      @keypress.enter="$emit('enter')"
+      ref="inputEl"
+      required
+    />
+    <div
+      class="input-field vuesax-ph"
+      :class="[
+        bgColor,
+        color,
+        {
+          focus: isInputFocus,
+          [errorPadding]: isError,
+        },
+      ]"
+      v-html="visibleValue"
+    />
+    <div
+      class="z-1 absolute left-0 top-0 h-full w-full pl-4 text-sm font-medium leading-10 transition-all duration-300"
+      :class="[
+        phColor,
+        {
+          'pl-5 !opacity-0': isInputFocus,
+          [errorPadding]: isError,
+        },
+      ]"
+      v-if="!value.length"
+    >
+      {{ placeholder }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = withDefaults(
+  defineProps<{
+    inputType: 'login' | 'password' | 'password1';
+    isPasswordVisible: boolean;
+    isDisabled: boolean;
+    isError: boolean;
+    placeholder: string;
+    autocomplete?: string;
+    initValue?: string;
+    focusBg?: string;
+    errorBg?: string;
+    disabledBg?: string;
+    disabledColor?: string;
+    placeholderColor?: string;
+    errorPadding?: string;
+    isSuccess?: boolean;
+    setFocusTrigger?: number;
+  }>(),
+  {
+    initValue: '',
+    autocomplete: 'username',
+    focusBg: 'bg-[#0d4c8582]',
+    errorBg: 'error-bg',
+    disabledBg: 'disabled-bg',
+    disabledColor: 'text-[#27425C]',
+    placeholderColor: 'text-[#4B7CA8]',
+    errorPadding: '',
+    isSuccess: false,
+  },
+);
+
+const emit = defineEmits<{
+  (e: 'change', value: string): void;
+  (e: 'focus'): void;
+  (e: 'blur'): void;
+  (e: 'enter'): void;
+}>();
+
+const value = ref('');
+
+const inputEl = ref<HTMLInputElement | null>(null);
+
+const isInputFocus = ref(false);
+
+const caretPosition = ref<number | null>(null);
+
+const caretEnd = ref<number | null>(null);
+
+const isFirefox = navigator.userAgent.search(/Firefox/i) >= 0;
+
+const dottedValue = computed(() => {
+  return '&#0149;'.repeat(value.value.length);
+});
+
+const doShowDots = computed(() => {
+  return props.inputType === 'password' && !props.isPasswordVisible;
+});
+
+const visibleValue = computed(() => {
+  if (!isInputFocus.value) return doShowDots.value ? dottedValue.value : value.value;
+  if (!value.value.length) return '<span class="blinking-cursor text-[#0d4c8582]">.</span>';
+  if (caretPosition.value === null) {
+    return doShowDots.value ? dottedValue.value : value.value;
+  }
+  const v = doShowDots.value
+    ? new Array(value.value.length).fill('&#0149;')
+    : value.value.split('');
+  v.splice(caretPosition.value, 0, '<span class="blinking-cursor"></span>');
+  if (caretEnd.value !== null) {
+    const cEnd = caretEnd.value <= value.value.length ? caretEnd.value : value.value.length;
+    v[caretPosition.value] = `${
+      v[caretPosition.value] || ''
+    }<span class="text-[#0F304B] bg-[#84B5E3]">`;
+    v[cEnd] += '</span>';
+  }
+  return v.join('');
+});
+
+const bgColor = computed(() => {
+  if (props.isDisabled) return props.disabledBg;
+  if (props.isError) return props.errorBg;
+  if (props.isSuccess) return 'bg-[#0C3F44]';
+  if (isInputFocus.value) return props.focusBg;
+  return 'bg-[#0F304B]';
+});
+
+const color = computed(() => {
+  if (props.isDisabled) return props.disabledColor;
+  if (props.isError) return 'text-[#F83068]';
+  if (props.isSuccess) return 'text-[#37C4A2]';
+  return 'text-[#84B5E3]';
+});
+
+const phColor = computed(() => {
+  if (props.isDisabled) return 'opacity-0';
+  if (props.isError) return 'text-[#F83068] opacity-30';
+  return props.placeholderColor;
+});
+
+watch(isInputFocus, () => {
+  if (isInputFocus.value) {
+    emit('focus');
+  } else {
+    emit('blur');
+  }
+});
+
+watch(value, () => {
+  emit('change', value.value);
+});
+
+watch(
+  () => props.initValue,
+  () => {
+    if (props.initValue !== value.value) value.value = props.initValue;
+  },
+  { immediate: true },
+);
+
+watch(
+  () => props.setFocusTrigger,
+  () => {
+    setTimeout(() => {
+      inputEl.value?.focus();
+    }, 10);
+  },
+);
+
+onMounted(() => {
+  if (isFirefox) {
+    if (inputEl.value) {
+      inputEl.value.addEventListener('select', selectEvent);
+    }
+  } else {
+    document.addEventListener('selectionchange', selectEvent);
+  }
+  // if (props.inputType === 'password') {
+  //   const unwatch = watch(value, () => {
+  //     unwatch();
+  //     forceUpdate();
+  //   });
+  // }
+});
+
+function selectEvent() {
+  if (!inputEl.value) return;
+  const { selectionStart, selectionEnd } = inputEl.value;
+  caretPosition.value = selectionStart;
+  caretEnd.value =
+    typeof selectionStart === 'number' &&
+    typeof selectionEnd === 'number' &&
+    selectionStart >= selectionEnd
+      ? null
+      : selectionEnd;
+}
+
+function gotFocus() {
+  if (!inputEl.value) return;
+  console.log('gotFocus');
+  if (inputEl.value.selectionStart === inputEl.value.selectionEnd) {
+    caretEnd.value = null;
+  }
+  setTimeout(() => {
+    isInputFocus.value = true;
+  }, 0);
+  const { selectionStart } = inputEl.value;
+  caretPosition.value = selectionStart;
+}
+
+function onBlur() {
+  setTimeout(() => {
+    isInputFocus.value = false;
+  }, 0);
+}
+</script>
+
+<style lang="postcss" scoped>
+.input-field {
+  @apply relative block
+      h-10 w-full overflow-hidden rounded-lg px-4 text-sm
+      font-medium leading-10 outline-none transition-all duration-150;
+  &.focus {
+    @apply pl-5;
+  }
+}
+.disabled-bg {
+  background: rgba(8, 13, 27, 0.42);
+}
+.error-bg {
+  background: rgba(125, 24, 60, 0.52);
+}
+</style>
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -13,14 +13,73 @@
         ></span>
         <span v-html="brand"></span>
       </div>
-      <div class="mt-6 text-base leading-[1.188] text-[#77b2d2]">
+      <div class="my-6 text-base leading-[1.188] text-[#77b2d2]">
         {{ t('title') }}
       </div>
+      <form
+        @submit.prevent
+        autocomplete="off"
+      >
+        <div
+          v-for="inputType in ['login', 'password'] as const"
+          :key="inputType"
+          class="relative first:mb-3"
+        >
+          <LoginInput
+            class="w-72"
+            :inputType="inputType"
+            :isPasswordVisible="isPasswordVisible"
+            :isDisabled="isDisabled"
+            :isError="isFieldError[inputType]"
+            :placeholder="t(`placeholder.${inputType}`)"
+            :setFocusTrigger="inputType === 'password' ? setFocusTrigger : 0"
+            @change="credentials[inputType] = $event"
+            @focus="inputType === 'password' ? (isPasswordFocus = true) : ''"
+            @blur="inputType === 'password' ? (isPasswordFocus = false) : ''"
+          />
+
+          <span
+            class="absolute bottom-0 right-full top-0 my-auto mr-4 h-1.5 w-1.5 scale-0 transform rounded bg-[#f83068] shadow-[0_0_0.25rem_0_#f83068] transition-transform on:scale-100"
+            :class="{ on: isFieldError[inputType] }"
+          ></span>
+          <button
+            v-if="inputType === 'password'"
+            type="button"
+            class="absolute bottom-0 right-3 top-0 my-auto z-[3] w-[1.125rem] h-[1.125rem]"
+            @click="
+              () => {
+                isPasswordVisible = !isPasswordVisible;
+                setFocusTrigger++;
+              }
+            "
+          >
+            <Transition
+              mode="out-in"
+              name="scale-y-150"
+            >
+              <span
+                v-html="isPasswordVisible ? openEye : closedEye"
+                :key="isPasswordVisible ? 'openEye' : 'closedEye'"
+                class="w-[1.125rem] h-[1.125rem] block group/icon"
+                :class="{ on: isPasswordFocus, error: isFieldError.password, off: isDisabled }"
+              ></span>
+            </Transition>
+          </button>
+        </div>
+        <div class="w-full text-center">
+          <button
+            class="rounded-lg h-[2.563rem] w-[10.125rem] bg-[#2d72fa] mt-[1.125rem] font-semibold text-sm leading-[1.214] hover:bg-[#3e7df9] active:bg-[#174cb6] disabled:bg-[#0e2b45] disabled:text-[#3e688e]"
+            :disabled="isDisabled || (isFieldError.login && isFieldError.password)"
+          >
+            {{ t('btn') }}
+          </button>
+        </div>
+      </form>
     </div>
     <div class="flex h-[2.75rem] w-full flex-row">
       <div class="w-[6.25rem] text-end">
         <button
-          v-for="(l, label) in { eng: 'en' as 'en', rus: 'ru' as 'ru' }"
+          v-for="(l, label) in { rus: 'ru' as 'ru', eng: 'en' as 'en' }"
           :key="l"
           class="ml-2 text-xs uppercase leading-[1.167] tracking-[0.083em] text-[#295c88] hover:text-[#2f8ee1] on:font-semibold on:text-[#45d2ff]"
           :class="{ on: locale === l }"
@@ -40,19 +99,76 @@
 <script lang="ts" setup>
 import brand from '@/assets/img/brand.svg?raw';
 import logo from '@/assets/img/logo.svg?raw';
+import openEye from '@/assets/img/open-eye.svg?raw';
+import closedEye from '@/assets/img/closed-eye.svg?raw';
+import LoginInput from '@/components/Ui/LoginInput.vue';
 
 const indexStore = useIndexStore();
 
-const isError = ref(false);
+const isFieldError = ref({
+  login: false,
+  password: false,
+});
+
+const isError = computed(() => {
+  return Object.values(isFieldError.value).some((v) => v);
+});
+
+const credentials = ref({
+  login: '',
+  password: '',
+});
+
+const setFocusTrigger = ref(0);
+
+watch(
+  () => credentials.value.login,
+  () => {
+    isFieldError.value.login = false;
+  },
+);
+
+watch(
+  () => credentials.value.password,
+  () => {
+    isFieldError.value.password = false;
+  },
+);
+
+const isPasswordVisible = ref(false);
+
+const isDisabled = ref(false);
+
+const isPasswordFocus = ref(false);
 
 const { locale } = useI18n();
 const { t } = useI18n({
   messages: {
     en: {
       title: 'Your login and password',
+      placeholder: {
+        login: 'Login',
+        password: 'Password',
+      },
+      btn: 'Log in',
+      msg: {
+        wrong: { header: 'Wrong login or password', text: 'Please try to log in again' },
+        disconnected: { header: 'The device offline', text: 'Check connection' },
+        connected: { header: 'The device online', text: 'Please try to log in again' },
+      },
     },
     ru: {
       title: 'Ваш логин и пароль',
+      placeholder: {
+        login: 'Логин',
+        password: 'Пароль',
+      },
+      btn: 'Войти',
+      msg: {
+        wrong: { header: 'Неправильный логин или пароль', text: 'Повторите авторизацию' },
+        disconnected: { header: 'Соединение отсутствует', text: 'Проверьте подключение' },
+        connected: { header: 'Соединение восстановлено', text: 'Повторите авторизацию' },
+      },
     },
   },
 });
