2945bc0457bdd57ced365fb187b2fac82924c77b
Valeria Masaltseva <valeriua.m@yandex.ru>
2023-11-21 11:13:52 +0300

    feat: add loader-line and update styles

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -2,6 +2,30 @@
     <div
         class="flex h-full w-full flex-col bg-[linear-gradient(108.08deg,#143554_-1.91%,#0a1b2b_98.91%)]"
     >
+        <transition name="loader-line">
+            <div
+                class="fixed top-0 left-0 h-1 w-full rounded transition-colors duration-150"
+                :class="{ 'bg-[#204261]': notConnected, 'bg-[#F83068]': isError && !isLoading }"
+                :style="{
+                    filter: notConnected ? 'drop-shadow(0px 2px 16px #071B2F)' : '',
+                }"
+            >
+                <div
+                    v-if="isLoading && !notConnected"
+                    class="absolute left-0 top-0 h-full transition-all duration-150 ease-linear"
+                    ref="time"
+                    :style="{
+                        'border-radius': '0 2px 2px 0',
+                        background:
+                            'linear-gradient(270deg, #8ECAFF 0.11%, #2298FF 13.77%, #2298FF 100.65%)',
+                        filter: 'drop-shadow(0px 4px 16px #003565)',
+                        transitionProperty: 'width',
+                        transitionDuration: `1s`,
+                        width: `${timeWidth}%`,
+                    }"
+                />
+            </div>
+        </transition>
         <div
             class="group flex flex-1 flex-col items-center justify-center"
             :class="{
@@ -88,12 +112,12 @@
                 </div>
             </form>
         </div>
-        <div class="flex h-[2.75rem] w-full flex-row">
-            <div class="w-[6.25rem] text-end">
+        <div class="flex h-[3.375rem] w-full flex-row">
+            <div class="w-[6.75rem] text-end">
                 <button
                     v-for="(l, label) in { rus: 'ru' as 'ru', eng: 'en' as 'en' }"
                     :key="l"
-                    class="ml-2 text-xs uppercase leading-[1.167] tracking-[0.083em] text-[#295c88] hover:text-[#2f8ee1] on:font-semibold on:text-[#45d2ff]"
+                    class="p-2.5 pl-[4px] pr-[4px] w-[2.125rem] text-xs uppercase leading-[1.167] tracking-[0.083em] text-[#295c88] hover:text-[#2f8ee1] on:font-semibold on:text-[#45d2ff]"
                     :class="{ on: locale === l }"
                     @click="indexStore.setLang(l)"
                 >
@@ -101,7 +125,7 @@
                 </button>
             </div>
             <div
-                class="flex-1 text-center text-xs leading-[1.167] tracking-[0.167em] text-[#295c88]"
+                class="flex-1 text-center text-xs leading-[1.167] tracking-[0.167em] text-[#295c88] pt-2.5"
             >
                 DESIGNED BY EVO CONTROLS
             </div>
@@ -147,6 +171,8 @@ const credentials = ref({
 
 const setFocusTrigger = ref(0);
 
+const timeWidth = ref<number>(0);
+
 watch(
     () => credentials.value.login,
     () => {
@@ -165,12 +191,18 @@ const isPasswordVisible = ref(false);
 
 const isDisabled = ref(false);
 
+const isLoading = ref(false);
+
 const isPasswordFocus = ref(false);
 
 const startLang = lang.value;
 
 async function login() {
     isDisabled.value = true;
+    isLoading.value = true;
+    setTimeout(() => {
+        timeWidth.value = 100;
+    }, 100);
     try {
         const r = await api.post('login', {
             name: credentials.value.login,
@@ -184,6 +216,8 @@ async function login() {
     } catch (error) {
         if (!notConnected.value) {
             isFieldError.value = { login: true, password: true };
+            isLoading.value = false;
+            timeWidth.value = 0;
             toast.error(t('msg.wrong.header'), t('msg.wrong.text'));
         }
     }
